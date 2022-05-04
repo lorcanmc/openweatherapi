85e8d55a9cb62a2cd9f9a26898b5e5921e78e22f
Lorcan Mc <lorcan.jmc@gmail.com>
2022-05-04 14:05:32 +0100

    started weather api stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [weatherData, setWeatherData] = useState(null)
+
+  useEffect(() => {
+    async function getRes() {
+      const res = await fetch(
+        "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e94c588742caca5ba8d6eac31c169521"
+      );
+      const data = await res.json();
+      setWeatherData(data)
+    }
+    getRes()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>{JSON.stringify(weatherData, null, 4)}</pre>
     </div>
   );
 }

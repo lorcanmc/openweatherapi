fb552542ed07029f46a835a259c0b070570fd8e1
Lorcan Mc <lorcan.jmc@gmail.com>
2022-05-04 21:04:27 +0100

    using current weather api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
-function App() {
+const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
-  const [weatherData, setWeatherData] = useState(null)
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [latLon, setLatLon] = useState(null);
+  const [location, setLocation] = useState("nuuk");
+  console.log(location, latLon, weatherData);
 
   useEffect(() => {
-    async function getRes() {
+    async function getWeatherData() {
+      if (location === null) return;
+      const geoRes = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`
+      );
+      const geoData = await geoRes.json();
+
       const res = await fetch(
-        "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e94c588742caca5ba8d6eac31c169521"
+        `https://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiKey}`
       );
       const data = await res.json();
-      setWeatherData(data)
+      setWeatherData(data);
     }
-    getRes()
-  }, []);
+    getWeatherData();
+  }, [location]);
+
+  if (weatherData === null) return <p>Loading</p>;
 
   return (
     <div className="App">
-      <pre>{JSON.stringify(weatherData, null, 4)}</pre>
+      <h1>{location[0].toUpperCase() + location.slice(1)}</h1>
+      <h2>{weatherData.weather[0].main}</h2>
+      <h2>temp: <span style={{color: (weatherData.main.temp - 273.15 > 0) ? "red" : "orange"}}>{Math.round(weatherData.main.temp - 273.15)}</span></h2>
+      <p>wind: {Math.round(weatherData.wind.speed)}mph</p>
     </div>
   );
 }
